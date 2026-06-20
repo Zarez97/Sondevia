41bfc140de0c77c213ada3f7d61b33362943e900
RicardoZ7 <76501400+Zarez97@users.noreply.github.com>
2026-06-20 17:00:29 -0600

    Update SecurityConfig.java

--- backend/src/main/java/com/proyecto_bad115/sistema_encuestas/config/SecurityConfig.java
+++ backend/src/main/java/com/proyecto_bad115/sistema_encuestas/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",
+                "https://sondevia.up.railway.app",
                 "https://sondevia-production.up.railway.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
